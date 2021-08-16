3e736cfa693dc5ccef2710fc580bdde5a1db1030
AndreySosnovyy <53178492+AndreySosnovyy@users.noreply.github.com>
2021-08-16 19:41:26 +0300

    adding bottom sheet

---
+++ lib/components/adding/adding_bottom_sheet.dart
@@ -0,0 +1,80 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AddingBottomSheet {
+  static Future show(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(28),
+            topRight: Radius.circular(28),
+          ),
+        ),
+        builder: (context) {
+          return Wrap(children: [
+            Column(
+              children: [
+                SizedBox(height: 20),
+                Container(
+                  height: 3,
+                  width: 120,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[700],
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.photo_camera_outlined,
+                    color: Colors.grey[700],
+                  ),
+                  title: Text(
+                    'Сфотографировать',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.image_outlined,
+                    color: Colors.grey[700],
+                  ),
+                  title: Text(
+                    'Добавить из галереи',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.list_alt,
+                    color: Colors.grey[700],
+                  ),
+                  title: Text(
+                    'Добавить список',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.label_outline,
+                    color: Colors.grey[700],
+                  ),
+                  title: Text(
+                    'Добавить тег',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ]);
+        }).whenComplete(() {});
+  }
+}
--- lib/routes/add_note_route.dart
+++ lib/routes/add_note_route.dart
@@ -1,3 +1,4 @@
+import 'package:briefy/components/adding/adding_bottom_sheet.dart';
 import 'package:briefy/components/adding/appbar_action_oval.dart';
 import 'package:briefy/model/note_model.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,8 @@ import 'package:flutter/services.dart';
 
 class AddNoteRoute extends StatelessWidget {
   AddNoteRoute(Level level);
+
+  var images = [];
 
   @override
   Widget build(BuildContext context) {
@@ -28,58 +31,131 @@ class AddNoteRoute extends StatelessWidget {
         centerTitle: true,
         actions: [
           appbarActionOval,
-          SizedBox(width: 6),
+          SizedBox(width: 14),
         ],
       ),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            SizedBox(height: 20),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          // ~~~~~~~~~~~~~~~~~ блок картинок ~~~~~~~~~~~~~~~~~
+          if (images.isNotEmpty)
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: TextField(
-                controller: noteTitleFieldController,
-                maxLength: 128,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+              height: 140,
+              margin: EdgeInsets.fromLTRB(16, 0, 16, 10),
+              child: ListView.separated(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: images.length,
+                itemBuilder: (BuildContext context, int index) => ClipRRect(
+                  clipBehavior: Clip.antiAlias,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    images[index],
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                decoration: InputDecoration(
-                  isDense: true,
-                  contentPadding: EdgeInsets.symmetric(vertical: 4.0),
-                  counterText: '',
-                  border: InputBorder.none,
-                  hintText: 'Название',
-                  hintStyle: TextStyle(color: Colors.grey),
+                separatorBuilder: (BuildContext context, int index) =>
+                    SizedBox(width: 10),
+              ),
+            ),
+          // ~~~~~~~~~~~~~~~~~ поле заголовка ~~~~~~~~~~~~~~~~~
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              controller: noteTitleFieldController,
+              maxLength: 128,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+              decoration: InputDecoration(
+                isDense: true,
+                contentPadding: EdgeInsets.symmetric(vertical: 4.0),
+                counterText: '',
+                border: InputBorder.none,
+                hintText: 'Название',
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+            ),
+          ),
+          // ~~~~~~~~~~~~~~~~~ поле текста ~~~~~~~~~~~~~~~~~
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: ScrollConfiguration(
+                behavior: NoGlowBehavior(),
+                child: TextField(
+                  controller: noteTextFieldController,
+                  maxLength: 2000,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    counterText: '',
+                    border: InputBorder.none,
+                    hintText: 'Текст',
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
                 ),
               ),
             ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: ScrollConfiguration(
-                  behavior: NoGlowBehavior(),
-                  child: TextField(
-                    controller: noteTextFieldController,
-                    maxLength: 2000,
-                    style: TextStyle(
-                      fontSize: 20,
+          ),
+          // ~~~~~~~~~~~~~~~~~ панель тегов ~~~~~~~~~~~~~~~~~
+          // todo: добавить теги (на pub.dev найти пакет для тегов)
+          // ~~~~~~~~~~~~~~~~~ нижняя панель ~~~~~~~~~~~~~~~~~
+          Row(
+            children: [
+              InkWell(
+                enableFeedback: false,
+                splashFactory: NoSplash.splashFactory,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Center(
+                    child: Icon(
+                      Icons.add_circle_outline,
+                      color: Colors.grey[700],
+                      size: 28,
                     ),
-                    maxLines: null,
-                    decoration: InputDecoration(
-                      counterText: '',
-                      border: InputBorder.none,
-                      hintText: 'Текст',
-                      hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+                onTap: () {
+                  FocusScope.of(context).unfocus();
+                  AddingBottomSheet.show(context);
+                },
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'Последнее изменение\nсегодня в 18:31',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
                     ),
                   ),
                 ),
               ),
-            )
-            
-          ],
-        ),
+              InkWell(
+                enableFeedback: false,
+                splashFactory: NoSplash.splashFactory,
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  child: Center(
+                    child: Icon(
+                      Icons.more_vert,
+                      color: Colors.grey[700],
+                      size: 28,
+                    ),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
